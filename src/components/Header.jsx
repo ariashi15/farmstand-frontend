b4afe1927a7f52b995b526c1d4f78efc14253c6d
chensycrystal <chensycrystal@gmail.com>
2025-04-05 15:36:49 -0500

    updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,25 @@
+import { ShoppingCart } from 'lucide-react';
+
 export default function Header() {
     return (
-        <header>
-            <nav class = "flex justify-between items-center bg-green-700 mx-auto">
+        <header class = "bg-green-700 py-3 items-center">
+            <nav class = "flex justify-between items-center bg-green-700 mx-auto w-[92%]">
                 <div>
                     <h5>Location</h5>
                 </div>
-                <div class="">
-                    <ul class= "flex items-right gap-[4vw]">
+                <div>
+                    <ul class= "flex items-center gap-[4vw]">
                         <li>
-                            <a class = "hover:text-gray-500" href = "#">Products</a>
+                            <a class = "hover:text-yellow-200" href = "#">Products</a>
                         </li>
                         <li>
-                            <a class = "hover:text-gray-500" href = "#">Farms</a>
+                            <a class = "hover:text-yellow-200" href = "#">Farms</a>
                         </li>
                     </ul>
                 </div>
                 <div class = "flex items-center gap-[4vw]">
-                    <button class = "btn">Login</button>
-                    <button class = "btn">Cart</button>
+                    <button class = "btn bg-green-200 rounded border-white px-4 py-1">Login</button>
+                    <button class = "btn  bg-green-200 rounded border-white px-4 py-1"><ShoppingCart size={24} /></button>
                 </div>
             </nav>
         </header>
