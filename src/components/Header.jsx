0178c0dbbc35b9272c8ec0d0fa4a83d60bb578dc
chensycrystal <chensycrystal@gmail.com>
2025-04-05 14:03:44 -0500

    started header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+    return (
+        <header>
+        <h1>Farmstand</h1>
+        <nav>
+            <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/products">Products</a></li>
+            <li><a href="/about">About</a></li>
+            </ul>
+        </nav>
+        </header>
+    )
+}
