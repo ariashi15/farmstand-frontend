1b7327a6df589eb9a3b3e0598c56b6870e50d172
Aria Shi <ariashi15@gmail.com>
2025-04-05 16:58:05 -0500

    finish outline for landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,6 +3,7 @@ import SearchBar from "../components/SearchBar"
 export default function LandingPage() {
     return (
     <>
+        {/* welcome banner */}
         <div class="flex flex-col justify-center items-center m-24">
             {/* background image*/}
             {/* <div class="absolute inset-0 bg-cover bg-center z-[-1] object-cover"
@@ -15,9 +16,21 @@ export default function LandingPage() {
                 Some subheading that we should decide later
             </h2>
             <SearchBar />
-            <button class="bg-light-yellow rounded-xl w-[250px] p-5 m-10 text-center">Explore Farms Near Me</button>
+            <button class="bg-light-yellow text-dark-yellow font-bold rounded-xl w-[250px] p-5 m-10 text-center">Explore Farms Near Me</button>
         </div>
-
+        {/* what's in season */}
+        <div class="flex flex-col justify-center items-center bg-dark-green p-10">
+            <h1 class="text-white text-4xl">What's In Season?</h1>
+            {/* food pics */}
+            <div class="flex justify-center gap-10 pt-10">
+                <div class="bg-light-gray h-24 w-24">placeholder</div>
+                <div class="bg-light-gray h-24 w-24">placeholder</div>
+                <div class="bg-light-gray h-24 w-24">placeholder</div>
+                <div class="bg-light-gray h-24 w-24">placeholder</div>
+                <div class="bg-light-gray h-24 w-24">placeholder</div>
+                <div class="bg-light-gray h-24 w-24">placeholder</div>
+            </div>
+        </div>
     </>
     )
 }
