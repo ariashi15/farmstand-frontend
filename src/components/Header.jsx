fd76cd4189ac140b93e7b8efa246d9fc2bf6b7be
Aria Shi <ariashi15@gmail.com>
2025-04-05 22:45:34 -0500

    added api call for getFarms

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,23 @@
 export default function Header() {
     return (
         <header>
-            <nav class = "flex justify-between items-center bg-green-700 mx-auto">
+            <nav className="flex justify-between items-center bg-green-700 mx-auto">
                 <div>
                     <h5>Location</h5>
                 </div>
-                <div class="">
-                    <ul class= "flex items-right gap-[4vw]">
+                <div>
+                    <ul className="flex items-right gap-[4vw]">
                         <li>
-                            <a class = "hover:text-gray-500" href = "#">Products</a>
+                            <a className="hover:text-gray-500" href="#">Products</a>
                         </li>
                         <li>
-                            <a class = "hover:text-gray-500" href = "#">Farms</a>
+                            <a className="hover:text-gray-500" href="#">Farms</a>
                         </li>
                     </ul>
                 </div>
-                <div class = "flex items-center gap-[4vw]">
-                    <button class = "btn">Login</button>
-                    <button class = "btn">Cart</button>
+                <div className="flex items-center gap-[4vw]">
+                    <button className="btn">Login</button>
+                    <button className="btn">Cart</button>
                 </div>
             </nav>
         </header>
